c68e39f818d5a08711ab229bfbb14ae189b63735
issac-sjps <issac@sjps.kh.edu.tw>
2026-03-22 15:58:00 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ⚠️ 改成你的 GitHub repo 名稱，例如 /quizflow/
-  base: '/quizflow/',
+  base: '/Sjps-QA-System/',
 })
